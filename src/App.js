bed6c6c75ce9231e50c5166a7c9e56c9209db345
tanmay <52064142+tanmay-kali@users.noreply.github.com>
2023-11-12 14:54:25 -0600

    prototype

--- src/App.js
+++ src/App.js
@@ -1,52 +1,123 @@
 import React, { useEffect, useState } from "react";
-import "./App.css"; // Make sure to include custom styles for animations in this file
+import "./App.css";
 import { Provider } from "@gadgetinc/react";
 import { api } from "./api/api";
-import gadget_bg from "./assets/gadget_bg.png"; // Ensure the asset is in the correct directory
+import gadget_bg from "./assets/gadget_bg.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [appName, setAppName] = useState("");
-  const [darkMode, setDarkMode] = useState(false); // State to toggle dark mode
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
-    // Fetch the app name using the API
-    api
-      .query(
-        `
-      query GetAppName {
-        gadgetMeta {
-          slug
-        }
-      }
-    `
-      )
-      .then((response) => {
-        if (response?.gadgetMeta?.slug) {
-          setAppName(response.gadgetMeta.slug);
-        }
-      })
+    // Load todos on component mount
+    api.todo
+      .findMany()
+      .then(setTodos)
       .catch((error) => {
-        console.error("Error fetching app name:", error);
+        showErrorWithMessage("Failed to load todos");
       });
   }, []);
 
-  const toggleDarkMode = () => setDarkMode(!darkMode); // Function to toggle dark mode
+  const showErrorWithMessage = (message) => {
+    setErrorMessage(message);
+    setShowError(true);
+    setTimeout(() => setShowError(false), 3000); // Hide after 3 seconds
+  };
 
-  // Define the base and mode-specific Tailwind CSS classes
+  const handleCreateOrUpdateTodo = async () => {
+    if (editingId) {
+      try {
+        await api.todo.update(4, { title: title });
+        setTitle(""); // Clear input after create/update
+        setEditingId(null); // Clear editing ID
+      } catch (error) {
+        showErrorWithMessage("Failed to update todo");
+      }
+    } else {
+      try {
+        await api.todo.create({ todo: { title } });
+        setTitle(""); // Clear input after create/update
+      } catch (error) {
+        showErrorWithMessage("Failed to create todo");
+      }
+    }
+    // Refresh todos
+    api.todo
+      .findMany()
+      .then(setTodos)
+      .catch((error) => {
+        showErrorWithMessage("Failed to refresh todos list");
+      });
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      await api.todo.delete(id);
+      setTodos(todos.filter((todo) => todo.id !== id)); // Update local state immediately
+    } catch (error) {
+      showErrorWithMessage("Failed to delete todo");
+    }
+  };
 
   return (
     <Provider api={api}>
-      <div className="App container  shadow-lg flex flex-col items-center space-y-4 bg-white text-gray-800">
-        <div>
-          <p className="text-lg">{appName || "Loading..."}</p>
+      <div className="App container shadow-lg flex flex-col items-center space-y-4 bg-white text-gray-800">
+        <h1 className="text-2xl text-gray-800 leading-tight font-bold">
+          ToDo List
+        </h1>
+        <div className="h-64  overflow-y-scroll w-full">
+          {todos.map((todo) => (
+            <div
+              key={todo.id}
+              className="flex justify-between items-center w-full p-2 hover:bg-gray-100"
+            >
+              <p className="flex-1 truncate">{todo.title}</p>
+              <button
+                onClick={() => {
+                  setTitle(todo.title);
+                  setEditingId(todo.id);
+                }}
+                className="text-blue-500 hover:text-blue-700"
+              >
+                <FontAwesomeIcon icon={faEdit} />
+              </button>
+              <button
+                onClick={() => handleDeleteTodo(todo.id)}
+                className="text-red-500 hover:text-red-700 ml-2"
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
+          ))}
         </div>
+        <input
+          type="text"
+          className="border border-gray-300 p-2 w-full"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleCreateOrUpdateTodo();
+            }
+          }}
+        />
+        {showError && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">Error: </strong>
+            <span className="block sm:inline">{errorMessage}</span>
+          </div>
+        )}{" "}
         <footer className="flex items-center">
-          {" "}
-          {/* Flex container, align items vertically */}
-          <h1 className="text-l font-bold mr-1">Made with Gadget</h1>{" "}
-          {/* Your existing h1 styling with right margin */}
-          <img src={gadget_bg} alt="Gadget logo" className="w-12" />{" "}
-          {/* w-12 is for width, you can adjust as needed */}
+          <h1 className="text-l font-bold mr-1">Made with Gadget</h1>
+          <img src={gadget_bg} alt="Gadget logo" className="w-12" />
         </footer>
       </div>
     </Provider>
--- src/api/api.js
+++ src/api/api.js
@@ -2,3 +2,23 @@ import { Client } from "@gadget-client/chrome-ext";
 export const api = new Client({
   authenticationMode: { browserSession: true },
 });
+
+// Fetch the app name using the Graphql API
+// api
+//   .query(
+//     `
+//   query GetAppName {
+//     gadgetMeta {
+//       slug
+//     }
+//   }
+// `
+//   )
+//   .then((response) => {
+//     if (response?.gadgetMeta?.slug) {
+//       setAppName(response.gadgetMeta.slug);
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching app name:", error);
+//   });
